34c62f26a5dbb940e975811c910d9cf910eaea16
Young-Hwan Mun <yh.msw9@gmail.com>
2015-01-03 00:22:15 -0500

    example completed.

--- src/Example/PinJoint.js
+++ src/Example/PinJoint.js
@@ -54,28 +54,28 @@ msw.PinJoint = msw.BaseScene.extend
 	    var 	BoxJoint  = cc.PhysicsJointDistance.create ( Box1 .getPhysicsBody ( ), Box2 .getPhysicsBody ( ), cc.p (  0, 0 ), cc.p (  50, 50 ) );
 	    this.getPhysicsWorld ( ).addJoint ( BallJoint );
 	    this.getPhysicsWorld ( ).addJoint ( BoxJoint  );
-	    
-	    /*
-	    Sprite* box = nullptr;
-	    PhysicsJointDistance* chainJoint_distance = nullptr;
-	    int chainLen = 10;
-	    Vector<Sprite*> boxes;
-	    for (int i = 0; i < chainLen; i++) {
-	        box = createBox(Point(500 + 40 * i, 600), Size(20, 10));
-	        this->addChild(box);
-	        boxes.pushBack(box);
-	        if (i == 0)
+	    	    
+	    var 	Box = null;
+	    var 	ChainJointDistance = null;
+	    var 	ChainLen = 10;
+	    var 	Boxes = new Array ( );
+	    for ( var i = 0; i < ChainLen; i++ )
+	    {
+	    	Box = this.createBox ( cc.p ( 500 + 40 * i, 600 ), cc.size ( 20, 10 ) );
+	        this.addChild ( Box );
+	        Boxes.push ( Box );
+	        if ( i == 0 )
 	        {
-	            box->getPhysicsBody()->setDynamic(false);
+	            Box.getPhysicsBody ( ).setDynamic ( false );
 	        }
-	        if(i > 0)
+	        
+	        if ( i > 0 )
 	        {
-	            chainJoint_distance = PhysicsJointDistance::construct(boxes.at(i-1)->getPhysicsBody(), boxes.at(i)->getPhysicsBody(), Point(10, 0), Point(-10, 0));
-	            chainJoint_distance->setDistance(10);
-	            _world->addJoint(chainJoint_distance);
+	        	ChainJointDistance = cc.PhysicsJointDistance.create ( Boxes [ i - 1 ].getPhysicsBody ( ), Boxes [ i ].getPhysicsBody ( ), cc.p ( 10, 0 ), cc.p ( -10, 0 ) );
+	        	ChainJointDistance.setDistance ( 10 );
+	        	this.getPhysicsWorld ( ).addJoint ( ChainJointDistance );
 	        }
-	    }	    
-	    */
+	    }	    	    
 	},
 	
 	demo_info:function ( )
